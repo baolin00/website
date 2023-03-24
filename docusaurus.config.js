9abf70823711fd25dec8411db57271ab47ebb2aa
bowling06 <128834270+bowling06@users.noreply.github.com>
2023-03-25 00:44:32 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,8 +12,6 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
